30f49b4fe208866c29d27b500f229ab4e02c5f07
yun <yun@yun-PC>
2015-01-21 18:18:59 +0900

    ..

--- beautyChuMobile/WebContent/js/main.js
+++ beautyChuMobile/WebContent/js/main.js
@@ -4,6 +4,20 @@ var maxPageNo;
 $(function() {
 	loadStyleList(1);
 
+	$('#style-detail').on('click', function() {
+		console.log('클릭');
+		$.mobile.changePage('#styledetail', {
+			dataUrl : "styledetail?styleno=2",
+			data : {
+				'styleno' : '2'
+			},
+			allowSamePageTransition : true,
+			transition : 'none',
+			showLoadMsg : false,
+			reloadPage : true
+		});
+	})
+
 	$(window).scroll(
 			function() {
 				if ($(window).scrollTop() >= $(document).height()
@@ -49,18 +63,21 @@ function loadStyleList(pageNo) {
 	$.getJSON('../json/style/list.do?pageNo=' + pageNo, function(data) {
 		console.log(data);
 		var styleList = data.styleList;
-		$('#styletemplate').load('../templates/styletemplate.html',
+		$('#styletemplate').load(
+				'../templates/styletemplate.html',
 				function() {
 					if (data.status === 'success') {
 						for (var i = 0; i < styleList.length; i++) {
 							var source = $('#styletemplate').html();
 							var template = Handlebars.compile(source);
 							var html = template(styleList[i]);
-							if ($('#content-section-1').outerHeight() <= $('#content-section-2').outerHeight()) {
+							if ($('#content-section-1').outerHeight() <= $(
+									'#content-section-2').outerHeight()) {
 								$('#content-section-1').fadeIn().append(html);
 							} else {
 								$('#content-section-2').fadeIn().append(html);
 							}
+							$('#styletemplate').trigger('create');
 						}
 					}
 
@@ -68,35 +85,39 @@ function loadStyleList(pageNo) {
 	});
 
 	currPageNo = ++pageNo;
-	console.log('다음페이지 : ' + currPageNo)
+	console.log('다음페이지 : ' + currPageNo);
 
 }
 
-$(document).on("pageinit", ".page", function() {
+$(document).on("pagebeforeshow", ".page", function() {
 	console.log('페이지 인잇');
 })
 
-$(document).on("pageinit", "#styledetail", function() {
-	console.log(location.href);
+$(document).on(		"pageinit",		"#styledetail",
+		function() {
+			console.log(location.href);
 
-	$(function() {
-		var mySwiper = new Swiper('.swiper-container', {
-			mode : 'horizontal',
-			loop : true,
-			grabCursor : true
-		})
+			$(function() {
+				var mySwiper = new Swiper('.swiper-container', {
+					mode : 'horizontal',
+					loop : true,
+					grabCursor : true
+				})
 
-		var url = location.href;
-		var styleNo=url.split("=");
-		
-		$.getJSON('../json/style/styledetail.do?styleNo=' + styleNo[styleNo.length-1], function(data) {
-			$('#styletemplate').on('load','../templates/styledetailtemplate.html',
-					function() {
+				var url = location.href;
+				var styleNo = url.split("=");
+
+				$.getJSON('../json/style/styledetail.do?styleNo='+ styleNo[styleNo.length - 1], function(data) {
+					$('#styletemplate').load('../templates/styledetailtemplate.html',
+							function() {
 								var source = $('#styletemplate').html();
 								var template = Handlebars.compile(source);
 								var html = template(data);
-									$('#styledetail').append(html);
-			});
+								$('#styledetail').append(html).trigger('create');
+
+					});
+				})
+			})
 		})
-	})
-})
+		
+		
